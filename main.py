d0bff42daddbfb3c751b10d427da3e3c2b72fb53
Titouan <titouan.dorier@outlook.fr>
2024-01-17 20:36:22 +0100

    ajout selection astre

--- main.py
+++ main.py
@@ -8,17 +8,41 @@ class MonApplication(QMainWindow):
     def __init__(self):
         super(MonApplication, self).__init__()
         
-        self.setWindowTitle("Page de connexion")
-        self.setWindowIcon()
-        
-        # Créer un widget central
-        self.windowContent = QWidget()
-        self.setCentralWidget(self.windowContent)
+        self.setWindowTitle("KSP trajectories")
+        self.setWindowIcon(QIcon("interface/logo.png"))
+        loadUi(localPath + "/interface/KSP.ui", self)
+        self.comboBoxAstreCible.addItem("Moho")
+        self.comboBoxAstreCible.addItem("Eve")
+        self.comboBoxAstreCible.addItem("Kerbin")
+        self.comboBoxAstreCible.addItem("Duna")
+        self.comboBoxAstreCible.addItem("Dres")
+        self.comboBoxAstreCible.addItem("Jool")
+        self.comboBoxAstreCible.addItem("Eeloo")
+        self.comboBoxAstreCible.currentIndexChanged.connect(self.getComboBoxAstreCibleChanged)
 
-        loadUi(localPath + "/interface/KSP.ui", self.windowContent)
+        self.comboBoxAstreDepart.addItem("Moho")
+        self.comboBoxAstreDepart.addItem("Eve")
+        self.comboBoxAstreDepart.addItem("Kerbin")
+        self.comboBoxAstreDepart.addItem("Duna")
+        self.comboBoxAstreDepart.addItem("Dres")
+        self.comboBoxAstreDepart.addItem("Jool")
+        self.comboBoxAstreDepart.addItem("Eeloo")
+        self.comboBoxAstreDepart.currentIndexChanged.connect(self.getComboBoxAstreDepartChanged)
 
+
+
+    def getComboBoxAstreCibleChanged(self):
+        selected_item = self.comboBoxAstreCible.currentText()
+        print(f"Sélection Astre Cible: {selected_item}")
+        return selected_item
+
+    def getComboBoxAstreDepartChanged(self):
+        selected_item = self.comboBoxAstreDepart.currentText()
+        print(f"Sélection Astre Départ: {selected_item}")
+        return selected_item
 
 localPath = os.path.dirname(os.path.abspath(__file__))
+
 
 if __name__ == '__main__': 
     os.system("cls")
